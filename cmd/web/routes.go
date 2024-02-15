1df3512224a599660e6518f240ba7b23084f8156
Benjamin Pöhlmann <benjamin_poehlmann@web.de>
2024-02-15 18:28:38 +0100

    finish chapter 6 middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,13 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"github.com/justinas/alice"
+	"net/http"
+)
 
-func (app *Application) initializeRoutes(config *Config) *http.ServeMux {
+func (app *Application) initializeRoutes(config *Config) http.Handler {
 	mux := http.NewServeMux()
+
 	fileServer := http.FileServer(http.Dir(config.staticDir))
 	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
-	return mux
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeader)
+	return standardMiddleware.Then(mux)
+	//return app.recoverPanic(app.logRequest(secureHeader(mux)))
 }
