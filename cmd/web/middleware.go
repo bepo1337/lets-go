8cf9282fbe48c10d285e6c253cb348dd31df8911
Benjamin Pöhlmann <benjamin_poehlmann@web.de>
2024-03-02 14:36:21 +0100

    review code and minor changes

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -12,7 +12,7 @@ import (
 func secureHeader(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
-		w.Header().Set("Refferer-Policy", "origin-when-cross-origin")
+		w.Header().Set("Referer-Policy", "origin-when-cross-origin")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "deny")
 		w.Header().Set("X-XSS-Protection", "0")
